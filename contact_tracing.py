de5b7dcaadbf722f43b504f3262b88977d11ffae
Maryloui Yesha Lacson <92633714+MarylouiYesha@users.noreply.github.com>
2022-11-12 15:14:01 +0800

    option 1

--- contact_tracing.py
+++ contact_tracing.py
@@ -7,20 +7,37 @@ print("")
 print("1 -> Add an item")
 print("2 -> Search")
 print("3 -> Exit (y/n)")
+print("")
+
 #Option 1
 
-user= int(input (" What do you want to do? (1-3"))
+user= int(input (" What do you want to do? (1-3)"))
 
-while True:
-    if user== 1:
-        name=input("Full Name: ")
-        age= int(input("Age: "))
-        add=input("Address: ")
-        phone= int(input("Phone Number: "))
-        print("Saved!")
-        
+if user== 1:
+    print("Add an item")
+    print("")
+
+    name=input("Full Name: ")
+    age= int(input("Age: "))
+    add=input("Address: ")
+    phone= int(input("Phone Number: "))
+
+    info={
+        "name_":{name},
+        "age-":{age},
+        "add_":{add},
+        "phone_":{phone}
+    }
+
+    print("Saved!")
 
 
 #Option 2
+
+if user==2:
+    print("Search")
+    print("")
+
+        
 #Option 3
  
