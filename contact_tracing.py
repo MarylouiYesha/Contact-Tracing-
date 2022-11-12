7c4ad3d0ba47a255fc92f81cb07dd05e6bf9e7d4
Maryloui Yesha Lacson <92633714+MarylouiYesha@users.noreply.github.com>
2022-11-12 14:51:16 +0800

    Add files via upload

--- contact_tracing.py
+++ contact_tracing.py
@@ -8,6 +8,19 @@ print("1 -> Add an item")
 print("2 -> Search")
 print("3 -> Exit (y/n)")
 #Option 1
+
+user= int(input (" What do you want to do? (1-3"))
+
+while True:
+    if user== 1:
+        name=input("Full Name: ")
+        age= int(input("Age: "))
+        add=input("Address: ")
+        phone= int(input("Phone Number: "))
+        print("Saved!")
+        
+
+
 #Option 2
 #Option 3
  
