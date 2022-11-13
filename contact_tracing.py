3a9b41d0f1bb3ebd00b40f194aa40ff958c493d4
Maryloui Yesha Lacson <92633714+MarylouiYesha@users.noreply.github.com>
2022-11-14 00:39:05 +0800

    Add files via upload

--- contact_tracing.py
+++ contact_tracing.py
@@ -11,46 +11,50 @@ print("")
 
 
 
-user= int(input (" What do you want to do? (1-3): "))
-
 information= {}
 
 
-#Option 1
-While = True
-if user== 1:
-    print("Add an item")
-    print("")    
-        
-    name=input("Full Name: ")
-    age= int(input("Age: "))
-    add=input("Address: ")
-    phone= int(input("Phone Number: "))
-        
-    information[name]=name
-    information[age]=age
-    information[add]=add
-    information[phone]=phone    
 
-    print("Saved!")
+
+#Option 1
+while True:
+    user= int(input (" What do you want to do? (1-3): "))
+    if user== 1:
+        print("Add an item")
+        print("")    
+        
+        name=input("Full Name: ")
+        age= int(input("Age: "))
+        add=input("Address: ")
+        phone= int(input("Phone Number: "))
+        
+        inputs={
+            "Name": name,
+            "Age": age,
+            "Address": add,
+            "Phone": phone
+        }  
+        
+        information[name]=inputs
+
+        print("Saved!")
     
     
 #Option 2
-if user==2:
-    print("Search")
-    
-    name2=input("What is your name? ")
-    if name2==information[name]:
-        print(information[age])
-        print(information[add])
-        print(information[phone]) 
+    if user==2:
+        print("Search")
+        name2=input("What is your name? ")
+        if name2 in information:
+            print(information[name2])
+        else:
+            print("No record")
                 
 #Option 3
-if user==3:
-    print("Exit")
-    print("")
-    exit1=input("Are you sure you want to exit (y/n)? ")
-    
-    if exit1=="y":
-        break
+    if user==3:
+        print("Exit")
+        print("")
+        exit1=input("Are you sure you want to exit (y/n)? ")
+  
+        if exit1=="y":
+            break
  
